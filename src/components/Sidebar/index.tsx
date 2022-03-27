808d3c6016f9f282003196372348845d9690fe73
mickeysss <sargsyanmikael@mail.ru>
2022-03-27 21:03:28 +0300

    created sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { NavLink } from 'react-router-dom';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import styles from './styles.module.scss';
+
+const Sidebar = () => {
+    return (
+        <div className={styles.sidebar}>
+            <div className={styles.logoContainer}>
+                <h1>Justice Cargos</h1>
+                <LocalShippingIcon />
+            </div>
+
+            <ul className={styles.navbar}>
+                <li>
+                    <NavLink to="/cargos">
+                        <span>Current Cargos</span>
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Sidebar;
