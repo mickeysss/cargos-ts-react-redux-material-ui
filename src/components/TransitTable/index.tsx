73c590090d28d4f9b41145611fdbf26c5879b12c
mickeysss <sargsyanmikael@mail.ru>
2022-03-29 08:22:07 +0300

    add table for transits

---
+++ src/components/TransitTable/index.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+import {
+    DataGrid,
+    GridColumns,
+    GridPreProcessEditCellProps,
+} from '@mui/x-data-grid';
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+type Props = {
+    selectedCargo: { [key: string]: any };
+    rowsInTransit: { [key: string]: any }[];
+};
+
+const TransitTable = ({ rowsInTransit }: Props) => {
+    return (
+        <div style={{ height: 400, width: '100%', color: 'white' }}>
+            <ThemeProvider theme={theme}>
+                <DataGrid
+                    rows={rowsInTransit}
+                    columns={columns}
+                    experimentalFeatures={{ newEditingApi: true }}
+                    onRowClick={(e: any) => console.log(e.row)}
+                />
+            </ThemeProvider>
+        </div>
+    );
+};
+
+const columns: GridColumns = [
+    {
+        field: 'destination',
+        preProcessEditCellProps: (params: GridPreProcessEditCellProps) => {
+            const hasError = params.props.value.length < 3;
+            return { ...params.props, error: hasError };
+        },
+        headerName: 'Destination',
+        width: 200,
+        editable: true,
+        align: 'left',
+        headerAlign: 'left',
+    },
+    {
+        field: 'status',
+        headerName: 'Status',
+        width: 200,
+        type: 'string',
+        align: 'left',
+        headerAlign: 'left',
+    },
+    {
+        field: 'name',
+        preProcessEditCellProps: (params: GridPreProcessEditCellProps) => {
+            const hasError = params.props.value.length < 3;
+            return { ...params.props, error: hasError };
+        },
+        headerName: 'Name',
+        width: 200,
+        editable: true,
+        align: 'left',
+        headerAlign: 'left',
+    },
+    {
+        field: 'category',
+        preProcessEditCellProps: (params: GridPreProcessEditCellProps) => {
+            const hasError = params.props.value.length < 3;
+            return { ...params.props, error: hasError };
+        },
+        headerName: 'Category',
+        width: 200,
+        type: 'string',
+        editable: true,
+        align: 'left',
+        headerAlign: 'left',
+    },
+    {
+        field: 'quantity',
+        preProcessEditCellProps: (params: GridPreProcessEditCellProps) => {
+            const hasError = params.props.value < 0;
+            return { ...params.props, error: hasError };
+        },
+        headerName: 'Quantity',
+        width: 200,
+        type: 'number',
+        editable: true,
+        align: 'left',
+        headerAlign: 'left',
+    },
+];
+
+export default TransitTable;
