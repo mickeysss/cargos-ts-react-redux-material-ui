024146f1b6c5668a4f87deda0d3b45ac2241a79f
mickeysss <sargsyanmikael@mail.ru>
2022-03-31 10:29:28 +0300

    added animation for sidebar logo

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import { NavLink } from 'react-router-dom';
+
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+
 import styles from './styles.module.scss';
 
 const Sidebar = () => {
@@ -9,20 +11,36 @@ const Sidebar = () => {
         <div className={styles.sidebar}>
             <div className={styles.logoContainer}>
                 <h1>Justice Cargos</h1>
-                <div className={styles.icon}>
-                    <LocalShippingIcon />
+                <div className={styles.road}>
+                    <div className={styles.icon}>
+                        <LocalShippingIcon />
+                    </div>
+                    <span className={styles.line} />
+                    <div className={styles.icon2}>
+                        <LocalShippingIcon />
+                    </div>
                 </div>
             </div>
 
             <ul className={styles.navbar}>
                 <li className={styles.navItem}>
                     <NavLink to="/">
-                    <span className={styles.icon}>Cargos</span>
+                        <span>Cargos</span>
                     </NavLink>
                 </li>
                 <li className={styles.navItem}>
                     <NavLink to="/transits">
                         <span>Transits</span>
+                    </NavLink>
+                </li>
+                <li className={styles.navItem}>
+                    <NavLink to="/completed">
+                        <span>Completed transits</span>
+                    </NavLink>
+                </li>
+                <li className={styles.navItem}>
+                    <NavLink to="/statistics">
+                        <span>Statistics</span>
                     </NavLink>
                 </li>
             </ul>
