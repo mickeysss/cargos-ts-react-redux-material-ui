3ed2d8be32e0c0f2e85897f8314facd927f0e2e8
mickeysss <sargsyanmikael@mail.ru>
2022-04-02 11:30:13 +0300

    connected redux persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-
-import { store } from './store';
+import { persistor, store } from './store';
 
 ReactDOM.render(
     <BrowserRouter>
         <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
             <App />
+            </PersistGate>
         </Provider>
     </BrowserRouter>,
     document.getElementById('root')
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,15 @@
-import { createStore } from 'redux';
-import { rootReducer } from './reducers';
+import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-export const store = createStore(rootReducer);
+import {rootReducer} from './reducers'
+
+const persistConfig = {
+    key: 'cargos',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer)
+export const persistor = persistStore(store)
